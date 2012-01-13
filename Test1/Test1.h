8628aa1bd1604848f84873ee1d877180ab1ad4a9
Jeremy Bunford <jbunford@ie.com>
2012-01-13 10:11:12 +0000

    Initial Commit

---
+++ Test1/Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  Test1
+//
+//  Created by Jeremy Bunford on 13/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
+
+@end
---
+++ Test1/Test1.m
@@ -0,0 +1,23 @@
+//
+//  Test1.m
+//  Test1
+//
+//  Created by Jeremy Bunford on 13/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
